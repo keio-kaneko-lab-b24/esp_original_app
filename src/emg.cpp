53d6ec74a15d8d9f6b9592a1211bada9387aef55
Tatsuya Mizuguchi <>
2021-11-07 17:02:09 +0900

    微修正

--- src/emg.cpp
+++ src/emg.cpp
@@ -22,7 +22,7 @@ void updataRMSFromString(
     // ExtensorのRMS値を取得
     while ((pos = value.find(delimiter)) != std::string::npos)
     {
-        std::string e_token = value.substr(3, 100); // 多めに100文字目まで取得
+        std::string e_token = value.substr(2, 100); // 多めに100文字目まで取得
         const char *e_str = e_token.c_str();
         extensor_value = atof(e_str);
         value.erase(0, pos + delimiter.length());
@@ -32,7 +32,7 @@ void updataRMSFromString(
     // FlexorのRMS値を取得
     while ((pos = value.find(delimiter)) != std::string::npos)
     {
-        std::string f_token = value.substr(4, 100); // 多めに100文字目まで取得
+        std::string f_token = value.substr(3, 100); // 多めに100文字目まで取得
         const char *f_str = f_token.c_str();
         flexor_value = atof(f_str);
         value.erase(0, pos + delimiter.length());
